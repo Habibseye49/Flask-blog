fe6ae3e41679cd6f3a8bdd689370c56210b2b3a1
Habibseye49 <habibseye49@gmail.com>
2025-04-22 14:09:13 +0100

    Auto-run migrations on startup for Render

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from flaskblog import create_app  # Import the create_app function from the flaskblog package
+from flask_migrate import upgrade
 
 app = create_app()  # Create an instance of the Flask application using the factory function
 
+with app.app_context():
+    upgrade()
+    
 if __name__ == '__main__':  # Ensure the app runs only when this script is executed directly
      app.run(debug=True)  # Run the Flask application in debug mode
